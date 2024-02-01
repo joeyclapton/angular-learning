b55f9c31eea7a7176cf40f83fc37239709934303
Joey Clapton <joeyclapton42@gmail.com>
2024-02-01 15:28:20 -0300

    feat: create avatar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProfileAvatarComponent } from './components/profile-avatar/profile-avatar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProfileAvatarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
