9821e0061905afc928d6d43da93559c80172b6f1
Joey Clapton <joeyclapton42@gmail.com>
2024-02-02 16:51:54 -0300

    feat: add header logo informations

--- client/src/app/components/templates/header/header.component.ts
+++ client/src/app/components/templates/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
