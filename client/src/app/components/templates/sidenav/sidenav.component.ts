d261552bce7ff081cdde9804afba2cee740deb2e
Joey Clapton <joeyclapton42@gmail.com>
2024-02-06 15:23:21 -0300

    style: adjust content layout order

--- client/src/app/components/templates/sidenav/sidenav.component.ts
+++ client/src/app/components/templates/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from '../../../views/home/home.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, HomeComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
