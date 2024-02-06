3b4a8018f7ac085d1f255ebf140be63451174848
Joey Clapton <joeyclapton42@gmail.com>
2024-02-06 15:41:08 -0300

    feat: add travels and home routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { TravelAddFormComponent } from './views/travel-add-form/travel-add-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'travels',
+    component: TravelAddFormComponent,
+  },
+];
--- client/src/app/components/templates/sidenav/sidenav.component.ts
+++ client/src/app/components/templates/sidenav/sidenav.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from '../../../views/home/home.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, HomeComponent],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    HomeComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
