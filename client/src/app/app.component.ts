1733971f5538f2aa5b3f11894e36091da87635ca
Joey Clapton <joeyclapton42@gmail.com>
2024-02-05 23:38:23 -0300

    refactor: remove service hello world

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,8 +4,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/templates/header/header.component';
 import { FooterComponent } from './components/templates/footer/footer.component';
 import { SidenavComponent } from './components/templates/sidenav/sidenav.component';
-import { CreateFruitComponent } from './create-fruit/create-fruit.component';
-import { CreateOrderComponent } from './create-order/create-order.component';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +14,6 @@ import { CreateOrderComponent } from './create-order/create-order.component';
     HeaderComponent,
     FooterComponent,
     SidenavComponent,
-    CreateFruitComponent,
-    CreateOrderComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
