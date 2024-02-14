110c1604e01a068d4f95ab563aef6a942c9c70e7
Joey Clapton <joeyclapton42@gmail.com>
2024-02-14 20:40:41 -0300

    refactor: remove unused import

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { TravelAddFormComponent } from './views/travel-add-form/travel-add-form.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { TravelCreateComponent } from './components/travels/travel-create/travel-create.component';
 
 export const routes: Routes = [
