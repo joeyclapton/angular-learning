af76ea8de85fde901edb439f843fdb3980aba73e
Joey Clapton <joeyclapton42@gmail.com>
2024-02-15 16:20:45 -0300

    feat: add logic to save a new travel

--- client/src/app/components/travels/travel-create/travel-create.component.ts
+++ client/src/app/components/travels/travel-create/travel-create.component.ts
@@ -3,11 +3,25 @@ import { TravelService } from '../../../shared/services/travel/travel.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { Travel } from '../../../shared/models/travel.model';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-travel-create',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDatepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './travel-create.component.html',
   styleUrl: './travel-create.component.scss',
 })
@@ -26,7 +40,13 @@ export class TravelCreateComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateTravel() {
-    this.travelService.create(this.travel).subscribe(() => {
+    const updatedTravel = {
+      ...this.travel,
+      stars: Number(this.travel.stars),
+    };
+    updatedTravel.stars = Number(updatedTravel.stars);
+
+    this.travelService.create(updatedTravel).subscribe(() => {
       this.travelService.showMessage('Viagem cadastrada com sucesso...');
       this.router.navigate(['/travels']);
     });
